281336e26d0f1b1ed17ab71b4a6638e4c7770689
Tal gvili <Gviligvili@gmail.com>
2017-07-16 03:52:33 -0700

    Semantic lodash and jquery

---
+++ src/app/modules/shared/test/demo-cmp.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+/**
+ * Created by 1 on 7/16/2017.
+ */
+@Component({
+  selector: 'demo-cmp',
+  template: `
+    <sm-segment class="raised">
+      Hello
+    </sm-segment>
+  `
+})
+export class DemoComponent {}
