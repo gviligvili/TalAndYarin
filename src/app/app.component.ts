ef1c7519fbc7fe5d4696eb548093baab7ec5071e
yarin <yarin@yarin.yarin>
2017-07-17 16:22:32 +0300

    change background to custom layer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {TargetService} from './target.service';
 import {Target} from './interfaces/target.interface';
+import {AgmMap} from '@agm/core';
 
 @Component({
   selector: 'app-root',
@@ -8,7 +9,7 @@ import {Target} from './interfaces/target.interface';
   styleUrls: ['./app.component.css'],
   providers: [TargetService]
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'Espek';
   initialLat = 32.073350;
   initialLon = 34.785941;
@@ -24,6 +25,27 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.getTargets();
+  }
+
+  @ViewChild(AgmMap) agmMap;
+  ngAfterViewInit(): void {
+    // Change background layer
+    this.agmMap.mapReady.subscribe(map => {
+      const osmMapType = new google.maps.ImageMapType({
+        getTileUrl: function (coord, zoom) {
+          return "http://tile.openstreetmap.org/" +
+            zoom + "/" + coord.x + "/" + coord.y + ".png";
+        },
+        tileSize: new google.maps.Size(256, 256),
+        isPng: true,
+        alt: "OpenStreetMap",
+        name: "OSM",
+        maxZoom: 19
+      });
+
+      map.mapTypes.set('OSM', osmMapType);
+      map.setMapTypeId('OSM');
+    });
   }
 
   getTargets(): void {
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -2,6 +2,7 @@
 declare var module: NodeModule;
 declare var _:any;
 declare var $:any;
+declare var google:any;
 
 interface NodeModule {
   id: string;
