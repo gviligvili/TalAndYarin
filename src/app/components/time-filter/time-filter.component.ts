7f9861546afee5d2211a7fa12ed4419ffc65c3ad
Tal gvili <Gviligvili@gmail.com>
2017-07-23 05:34:36 -0700

    Everything is synced, and services works great.

--- src/app/components/time-filter/time-filter.component.ts
+++ src/app/components/time-filter/time-filter.component.ts
@@ -1,7 +1,41 @@
 import {Component, OnInit} from '@angular/core';
-import {IMyDateModel, IMyDpOptions} from "mydatepicker";
-import * as moment from "moment";
+import {IMyDateModel, IMyDpOptions} from 'mydatepicker';
+import * as moment from 'moment';
+import {NouiFormatter} from "ng2-nouislider";
 
+export class TimeFormatter implements NouiFormatter {
+  to(value: number): string {
+    console.log("1) To : ", value);
+    const h = Math.floor(value / 3600000);
+    const m = Math.floor(value % 3600000 / 60000);
+    const values = [h, m];
+    let timeString: string = '';
+    let i = 0;
+    for (let v of values) {
+      if (values[i] < 10) {
+        timeString += '0';
+      }
+
+      timeString += values[i].toFixed(0);
+      if (i < 1) {
+        timeString += ':';
+      }
+      i++;
+    }
+    console.log("2) To : ", timeString);
+    return timeString;
+  };
+
+  from(value: string): number {
+    console.log("1) From : ", value);
+    let v = value.split(':').map(parseInt);
+    let time: number = 0;
+    time += v[0] * 3600000;
+    time += v[1] * 60000;
+    console.log("2) From : ", time);
+    return time;
+  }
+}
 
 @Component({
   selector: 'time-filter',
@@ -14,9 +48,16 @@ export class TimeFilterComponent {
     behaviour: 'drag',
     connect: true,
     margin: 1,
+    range: {
+      min: 0,
+      max: 86400000,
+    },
     pips: {
-      mode: 'steps',
-      density: 5
+      mode: 'values',
+      values: [0, 3600000, 7200000, 10800000, 14400000, 18000000, 21600000, 25200000, 28800000, 32400000, 36000000, 39600000, 43200000, 46800000, 50400000, 54000000, 57600000, 61200000, 64800000, 68400000, 72000000, 75600000, 79200000, 82800000, 86400000],
+      density: 1,
+      stepped: true,
+      format: new TimeFormatter()
     },
   };
 
@@ -53,13 +94,7 @@ export class TimeFilterComponent {
 
   updateSlider() {
     console.log([this.selectedDate.getTime(), moment(this.selectedDate).add(1, 'day').valueOf()])
-    this.rangeArr = [this.selectedDate.getTime(), moment(this.selectedDate).add(1, 'day').valueOf()];
-    this.currRange = [this.selectedDate.getTime(), moment(this.selectedDate).add(1, 'day').valueOf()];
-
-    // I'm doing it because of some stupid race condition.
-    let self = this;
-    setTimeout(() => {
-      self.currRange = [self.selectedDate.getTime(), moment(self.selectedDate).add(1, 'day').valueOf()];
-    }, 200);
+    // this.rangeArr = [this.selectedDate.getTime(), moment(this.selectedDate).add(1, 'day').valueOf()];
+    this.currRange = [1, 200];
   }
 }
