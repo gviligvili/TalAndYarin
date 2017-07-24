36a3d496ea83c5624465a525aee9f8b121c45546
Tal Gvili <gviligvili@gmail.com>
2017-07-24 04:17:50 -0700

    Merge branch 'master' of https://github.com/yarinr/TalAndYarin
    # Conflicts:
    #	src/app/app.component.ts
    #	src/app/components/cluster-list/cluster-item/cluster-item.component.ts
    #	src/app/components/cluster-list/target-item/target-item.component.ts

--- src/app/components/side-menu/side-menu.component.ts
+++ src/app/components/side-menu/side-menu.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {MenuService} from '../../services/menu-service/menu.service';
 import {MenuState} from '../../services/menu-service/menu-state.enum';
+import {animate, style, trigger, transition} from "@angular/animations";
+
+// const showingStyle =
 
 @Component({
   selector: 'side-menu',
   templateUrl: './side-menu.component.html',
-  styleUrls: ['./side-menu.component.scss']
+  styleUrls: ['./side-menu.component.scss'],
+  animations: [
+    trigger('SlideDrawerAnimation', [
+      transition(':enter', [
+        style({
+          opacity: 0,
+          width: '0px',
+          overflow: 'hidden'
+        }),
+        animate(200, style({
+          opacity: 1,
+          width: '350px',
+}))
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 1,
+          width: '350px',
+        }),
+        animate(200, style({
+          opacity: 0,
+          width: '0px',
+          overflow: 'hidden'
+        }))
+      ]),
+    ])
+  ]
 })
 export class SideMenuComponent {
 
